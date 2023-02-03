f8b16409144528120a4a4a94cc26c8ce875f8000
Clown Supreme <zachary.h.shedd@gmail.com>
2023-02-02 18:09:07 -0800

    Added notes.js post routes and const.

---
+++ assets/js/notes.js
@@ -0,0 +1,22 @@
+const noteRoute = require('express').Router();
+const storeJS = require('./store.js');
+
+noteRoute.get('/notes', (req, res) => {
+  storeJS
+  .getAllNotes().then((data) => res.json(data));
+});
+
+// POST Route for a new UX/UI note
+noteRoute.post('/notes', (req, res) => {
+  storeJS
+  .addNewNote(req.body).then((note) => res.json(note))
+  .catch((err) => res.status(500).json(err));
+});
+
+noteRoute.delete('/notes/:id', (req, res) => {
+  storeJS
+  .removeNoteId(req.params.id).then(() => res.json({ok: true}))
+  .catch((err) => res.status(500).json(err));
+})
+
+module.exports = noteRoute;
--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-//middleware
+//middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/', html);
