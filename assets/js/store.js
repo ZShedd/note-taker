90f6624777c168628f7c44fc34ad4474105cbed7
Clown Supreme <zachary.h.shedd@gmail.com>
2023-02-02 19:15:50 -0800

    Added newnote, removenote to store.js

--- assets/js/store.js
+++ assets/js/store.js
@@ -24,3 +24,31 @@ class StoredNotes {
             return parsedNotes;
         })
     };
+
+addNewNote(note) {
+        const { title, text } = note;
+
+        if (!title || !text) {
+            throw new Error('Please add a title and some text to your note.');
+        }
+
+        const nextNote = {
+            title,
+            text,
+            id: uuidv4(),
+        };
+        
+        return this.getAllNotes()
+        .then((notes) => [...notes, nextNote])
+        .then((savedNotes) => this.write(savedNotes))
+        .then(() => nextNote);
+    }
+
+    removeNoteId(id) {
+        return this.getAllNotes()
+        .then((notes) => notes.filter((note) => note.id!== id))
+        .then((justNotes) => this.write(justNotes))
+    }
+}
+
+module.exports = new StoredNotes();
